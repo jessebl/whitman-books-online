d9516842578e57288de812bf44c898f9774aa63a
Jesse Bulson-Lewis <bulsonjg@whitman.edu>
2018-04-24 11:03:50 -0700

    Switch from SQLite to MySQL if specific environement variable is set
    If WBO_PRODUCTION_DB==1 in the environment, then the the backend service
    will use the given MySQL database on localhost.
    
    This commit also sets that environment variable accordingly for the
    backend service through systemd. Use the deployment code to make use of
    this.

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -6,6 +7,16 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 
 app = Flask(__name__)
+if os.getenv('WBO_PRODUCTION_DB') == '1':
+    # Relies on credentials in ~/.my.cnf to log into DB **OR**
+    # Null password for backend user
+    # 'backend' is MySQL user
+    # 'localhost' is MySQL server
+    # 'wbo' is the MySQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://backend:localhost/wbo'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #turns off Flask-SQL Alchemy modification tracker, not underlying SQLAlchemy modification tracker
+    pass
+else:
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #turns off Flask-SQL Alchemy modification tracker, not underlying SQLAlchemy modification tracker
 app.secret_key = "Sean"
