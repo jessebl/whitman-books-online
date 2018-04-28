70bc95c739c4d4523e958ac35e9d519e0b5b01c7
Jesse Bulson-Lewis <bulsonjg@whitman.edu>
2018-04-28 01:17:51 -0700

    Use verify_encoded_token() for verification and token decoding
    Not only is the token itself verified, but the domain of the token is
    checked against a list of legitimate domains (currently only
    whitman.edu)

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -1,55 +1,45 @@
 from google.oauth2 import id_token
 from google.auth.transport import requests
 
-def get_user_email(user_token):
-    """Determine the email of the given Google Oauth2 token
+client_ids = (
+    "317596678792-2ekdkdrdlgsqdaudaag7t7m7qf4m0b17.apps.googleusercontent.com")
+authorized_email_domains = ("whitman.edu")
 
-    Gets the email address from Google and also verifies the token. Exception
-    is raised if an email isn't found for whatever reason or if token
-    verification fails.
+
+def verify_encoded_token(encoded_token):
+    """Verify and get the client's token
+
+    Verifies the token's signature, expiration, that it is from the API created
+    by the front end, that the email domain is one from
+    ``authorized_email_domains`` (whitman.edu), and that Google issued it.
 
     Args:
-        user_token (str): The client token to verify and get email from
+        encoded_token (str): The client token to verify and get email from
 
     Returns:
-        str: Email address
-
-    Raises:
-        ValueError: No valid email found
-
+        dict: validated token
+        NoneType: if token is invalid for whatever reason
     """
+    # Modified from
+    # https://developers.google.com/identity/sign-in/web/backend-auth
     try:
-        decoded_token = verify_user_token(user_token)
+        # Client ID of the Google API (found in Developer Console)
+
+        # Raises ValueError if token is invalid
+        decoded_token = id_token.verify_oauth2_token(
+            encoded_token, requests.Request())
+
+        if decoded_token["aud"] not in [client_ids]:
+            raise ValueError("Could not verify audience (API client ID)")
+
+        if decoded_token["hd"] not in authorized_email_domains:
+            raise ValueError("Domain of token not authorized")
+
+        if decoded_token["iss"] not in [
+                "accounts.google.com", "https://accounts.google.com"]:
+            raise ValueError("Token not issued by Google")
+
     except ValueError:
-        raise ValueError('No valid email found')
-
-    email = decoded_token['email']
-
-    return email
-
-def verify_user_token(user_token):
-    """Verify that the user's token is legit
-
-    Verifies the JWT signature, the aud claim, and the exp claim
-
-    Args:
-        user_token (str): The client token to verify and get email from
-
-    Returns:
-        Decoded JWT
-
-    Raises:
-        ValueError: Token is invalid
-    """
-    # Modified from https://developers.google.com/identity/sign-in/web/backend-auth
-
-    # Client ID of the Google API
-    CLIENT_ID = 'whitman-books'
-
-    # Raises ValueError if token is invalid
-    decoded_token = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
-    # Raises ValueError if token isn't from Google
-    if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-        raise ValueError('Wrong issuer.')
+        return None
 
     return decoded_token
